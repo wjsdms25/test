46934128d88d21933c1c6c2fbd0f166a7395acb6
yeeun <21500631@handong.edu>
2019-07-24 10:38:30 +0900

    asdf

--- flutter_app_07_22/lib/main.dart
+++ flutter_app_07_22/lib/main.dart
@@ -46,162 +46,97 @@ class _HelloPageState extends State<HelloPage> {
                 height: double.infinity,
                 decoration: BoxDecoration(
                     image: new DecorationImage(
-                        image: new AssetImage('image/main_background.png'),
+                        image: new AssetImage('image/main_img.jpeg'),
                         fit: BoxFit.cover)
                 ),
-                child: Padding(
-                    padding: const EdgeInsets.only(left:110.0,top: 1.0),
+                //child: Padding(
+                    //padding: const EdgeInsets.only(top: 1.0),
                     child: Column(
                         children: <Widget>[
-                          Expanded(
-                              flex: 1,
-                              child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      width: 210.0,
-                                      child: Image.asset(
-                                          'image/flipclip_image.png'
-                                      ),
-                                    ),
-                                    MaterialButton(
-                                        child: Image.asset('image/plus_shadow.png'),
-                                        onPressed: _PlusPressed
-                                    )
-                                  ]
 
-                                //            ));
-                              )
-                          ),
-                          Expanded(
-                            flex:5,
-                            child:Container()
+                          SizedBox(height: 150.0),
+                          SizedBox(
+                              height: 100.0,
 
-                )
-                        ]
-                    )
-                  //Container(
-
-                  //)
-                )
-            )
-          ],
-        )
-    );
-  }
-
-//              child: Column(
-//                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//
-//                  children: <Widget>[
-//                    Expanded(
-//                       flex: 1,
-//                      child:
-//                      Container(
-//                        width : 180.0,
-//                        child: Image.asset('image/flipclip_image.png' ),
-//                      ),
-//                    ),
-//
-//                    Expanded(
-//                    flex:5,
-//                        child: Container(),
-//                    )
-//                  ]
-//              )
-
-//                    Padding(
-//                      padding: const EdgeInsets.all(0.0),
-//                      child: MaterialButton(
-//                        child: Image.asset('image/plus_shadow.png'),
-//                        onPressed:_PlusPressed
-//                      )
-//                      )
- //   ]
-   //                 )
-    //            )
-      //      ),
-        //  ]
-
-                        //mainAxisAlignment: MainAxisAlignment.end,
-                        //children: <Widget> [
-                      //padding: const EdgeInsets.all(2.0),
-//            materialbutton: MaterialButton(
-//                          child: Image.asset('plus_shadow.png'),
-//                          onPressed: _PlusPressed
-
-          //            ));
-                   // ]
+                               child: new RaisedButton(onPressed: null),
+                                //border: Border(bottom: BorderSide()),
 
 
+                             // RaisedButton(onPressed: null)
 
+                      ),
+                          Container(
+                            //onPressed
+                              height: 100.0,
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide()),
+                              )),
+                          Container(
+                              height: 100.0,
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide()),
+                              )),
+                          Container(
+                              height: 100.0,
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide()),
+                              )),
+                        ],))
 
-
-
-
-
-
-
-
-//        )
-//
-//
-//
-//          ],
-//        )
-//
-//    );
-
-
-  void _PlusPressed() {
-
-      showDialog(
-          context: context, builder: (BuildContext context) {
-        return AlertDialog(
-          content: Form(
-            key: _formKey,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-//                Image.asset(
-//                'image/paint.png',
-//            ),
-              Padding(
-                padding: const EdgeInsets.all(2.0),
-
-                  child: MaterialButton(
-                    child: Image.asset('image/paint.png'),
-                    onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => InputImage())
+            ])
                     );
 
-//                    if (_formKey.currentState.validate()) {
-//                      _formKey.currentState.save();
-//                    }
-                  },
-                  )
-          ),
-              Padding(
-                padding: const EdgeInsets.all(2.0),
-                    child: MaterialButton(
-                      child: Image.asset('image/text.png'),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => InputText())
-                      );
-                        if (_formKey.currentState.validate()) {
-                          _formKey.currentState.save();
-                        }
-                      },
-                    ),
-                )
-              ],
-            ),
-          ),
-        );
-      });
-    }
+
+
   }
 
+//
+//  void _PlusPressed() {
+//
+//      showDialog(
+//          context: context, builder: (BuildContext context) {
+//        return AlertDialog(
+//          content: Form(
+//            key: _formKey,
+//            child: Row(
+//              mainAxisSize: MainAxisSize.min,
+//              children: <Widget>[
+//
+//              Padding(
+//                padding: const EdgeInsets.all(2.0),
+//
+//                  child: MaterialButton(
+//                    child: Image.asset('image/paint.png'),
+//                    onPressed: () {
+//                    Navigator.push(
+//                        context,
+//                        MaterialPageRoute(builder: (context) => InputImage())
+//                    );
+//
+//
+//                  },
+//                  )
+//          ),
+//              Padding(
+//                padding: const EdgeInsets.all(2.0),
+//                    child: MaterialButton(
+//                      child: Image.asset('image/text.png'),
+//                      onPressed: () {
+//                        Navigator.push(
+//                          context,
+//                          MaterialPageRoute(builder: (context) => InputText())
+//                      );
+//                        if (_formKey.currentState.validate()) {
+//                          _formKey.currentState.save();
+//                        }
+//                      },
+//                    ),
+//                )
+//              ],
+//            ),
+//          ),
+//        );
+//      });
+//    }
+//  }
+//
